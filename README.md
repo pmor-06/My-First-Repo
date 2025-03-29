20ea68bdf1cd184d9a599f2e91a02912a1feede4
pmor-06 <briantpmorenop@gmail.com>
2025-03-29 12:03:32 -0600

    adding new changes to app.py and EDA.ipynb and README.md files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # My-First-Repo
 Tripleten Data Science Project 7 Repository
+Construir una aplicación que muestre tres gráficos para la comparación de dos columnas en el dataframe de vehículos de US
--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # Read csv data file
 
-st.header('Construir un histograma') # Create a header to explain the app
+st.header('Construye hasta tres gráficos para comparar los valores de dos columnas diferentes del DataFrame vehicles_us.cvs') # Create a header to explain the app
 
-hist_button = st.button('Construir un histograma') # Create the button
+hist_button = st.button('Construir un histograma sobre la distancia total recorrida de los carros') # Create the button
 
 if hist_button: # If click in the button
     st.write('Creación de un histograma para el conjunto de datos de anuncios de ventas de vehículos de coches en Estados Unidos')
@@ -15,7 +15,7 @@ if hist_button: # If click in the button
     fig.show() # Show graphic
     st.plotly_chart(fig, use_container_width=True) # Show an interactive chart
 
-dis_button = st.button('Construir un gráfico de dispersión')
+dis_button = st.button('Construir un gráfico de dispersión sobre la dependencia entre el valor del automóvil y la distancia total recorrida del carro')
 
 if dis_button: # If click in the button
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de ventas de vehículos de coches en Estados Unidos')
@@ -23,3 +23,11 @@ if dis_button: # If click in the button
     fig = px.scatter(car_data, x="odometer", y="price") # Create an dispersion graph
     fig.show() # Show the graph
     st.plotly_chart(fig, use_container_width=True) # Show an interactive chart
+
+build_hist = st.checkbox('Construir un histograma sobre el valor de los automóviles')
+
+if build_hist:
+    st.write('Construir un histograma para la columna de precio')
+
+    fig = px.histogram(car_data, x = 'price') # crear un histograma sobre el precio
+    fig.show() # mostrar histograma
